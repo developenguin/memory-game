e2124fcd1af36b8c9c78847caf998e528f269b12
Developenguin <matthijs@heavymetalpenguin.com>
2017-12-29 12:56:26 +0100

    Replace shuffle function with Lodash shuffle

--- js/app.js
+++ js/app.js
@@ -455,22 +455,8 @@ function populateCardList() {
   return result;
 }
 
-// Shuffle function from http://stackoverflow.com/a/2450976
 function shuffleCards(array) {
-  let currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
-
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-
+  return _.shuffle(array);
 }
 
 // Start the first game of the session
